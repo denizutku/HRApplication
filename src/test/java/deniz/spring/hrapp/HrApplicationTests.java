3a5709cd542869b3542ad64b3e9ac52d2c049c1f
Deniz Utku Beydogan <beydogandeniz@gmail.com>
2019-11-08 16:39:42 +0300

    First commit

---
+++ src/test/java/deniz/spring/hrapp/HrApplicationTests.java
@@ -0,0 +1,13 @@
+package deniz.spring.hrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
